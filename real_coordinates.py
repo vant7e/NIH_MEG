ade70a895694fb7fd99b731d957b6b00e856080a
vant7e <144391925+vant7e@users.noreply.github.com>
2023-10-04 02:06:56 -0400

    Update real_coordinates.py

--- real_coordinates.py
+++ real_coordinates.py
@@ -5,16 +5,16 @@ import json
 data = pd.read_csv('/Users/Vante/Library/CloudStorage/OneDrive-UniversityofToronto/Meltzer Lab/NIH_MEG/sc_cate/object_task_train_10_VVG_anonated.csv')
 
 # Load the class-descriptions-boxable.csv file
-class_descriptions = pd.read_csv('/Users/Vante/Library/CloudStorage/OneDrive-UniversityofToronto/Meltzer Lab/NIH_MEG/sc_cate/class-descriptions-boxable.csv', header=None, names=['LabelName', 'Class'])
+class_descriptions = pd.read_csv('/Users/Vante/Library/CloudStorage/OneDrive-UniversityofToronto/Meltzer Lab/NIH_MEG/sc_cate/class-descriptions-boxable.csv', names=['LabelName', 'Class'])
 
 # Assuming the 'region_shape_attributes' column contains JSON strings in CSV
 data['region_shape_attributes'] = data['region_shape_attributes'].apply(json.loads)
 
 # Extract XMin, XMax, YMin, YMax into separate columns
 data['XMin'] = data['region_shape_attributes'].apply(lambda x: x.get('x'))
-data['XMax'] = data.apply(lambda row: row['XMIN'] + row['region_shape_attributes'].get('width', 0), axis=1)
+data['XMax'] = data.apply(lambda row: row['XMin'] + row['region_shape_attributes'].get('width', 0), axis=1)
 data['YMin'] = data['region_shape_attributes'].apply(lambda x: x.get('y'))
-data['YMax'] = data.apply(lambda row: row['YMIN'] + row['region_shape_attributes'].get('height', 0), axis=1)
+data['YMax'] = data.apply(lambda row: row['YMin'] + row['region_shape_attributes'].get('height', 0), axis=1)
 
 # Drop the original 'region_shape_attributes' column
 data.drop(columns=['region_shape_attributes'], inplace=True)
@@ -22,8 +22,8 @@ data.drop(columns=['region_shape_attributes'], inplace=True)
 # Assuming the 'region_attributes' column contains JSON strings in CSV
 data['region_attributes'] = data['region_attributes'].apply(json.loads)
 
-# Extract the "class_name" value and create a new column named "class"
-data['class_name'] = data['region_attributes'].apply(lambda x: x.get('class'))
+# Drop the "class_name" key from 'region_attributes' while keeping the rest of the JSON
+data['LabelName'] = data['region_attributes'].apply(lambda x: x.get('class_name'))
 
 # Merge with class descriptions to get the "class" column
 data = data.merge(class_descriptions, on='LabelName', how='left')
@@ -31,10 +31,8 @@ data = data.merge(class_descriptions, on='LabelName', how='left')
 # Drop the original 'region_attributes' column
 data.drop(columns=['region_attributes'], inplace=True)
 
-# Drop the 'region_count' and 'region_id' columns
-data.drop(columns=['region_count', 'region_id'], inplace=True)
-
 # Save the modified DataFrame back to a CSV file
 data.to_csv('output_data.csv', index=False)
+
 
 
